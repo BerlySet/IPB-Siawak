432f32688f109267206833776e7d0fd1b2813cac
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2021-06-14 16:09:09 +0700

    Dummy tabel Rec

--- resources/views/recruit/recruitments.blade.php
+++ resources/views/recruit/recruitments.blade.php
@@ -14,26 +14,55 @@
 
                 <table class="table my-3">
                     <thead class="table-info">
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Nama Acara</th>
-                        <th scope="col">Waktu Pembukaan</th>
-                        <th scope="col">Waktu Penutupan</th>
-                        <th scope="col">Status</th>
-                    </tr>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama Acara</th>
+                            <th scope="col">Waktu Pembukaan</th>
+                            <th scope="col">Waktu Penutupan</th>
+                            <th scope="col">Status</th>
+                        </tr>
                     </thead>
         
                     <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Panitia MPKMB 59</td>
+                            <td>16-02-2022</td>
+                            <td>16-03-2022</td>
+                            <td>
+                            <button type="button" class="btn btn-success btn-sm">Completed</button>
+                            </td>
+                        </tr>
 
-                    </tr>
-                    <td>1</td>
-                    <td>Panitia MPKMB 59</td>
-                    <td>16-02-2022</td>
-                    <td>16-03-2022</td>
-                    <td>
-                    <button type="button" class="btn btn-success btn-sm">Complete</button>
-                    </td>
-                    <tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Oprec Staff Seruni 2022</td>
+                            <td>03-07-2022</td>
+                            <td>17-07-2022</td>
+                            <td>
+                            <button type="button" class="btn btn-warning btn-sm">Upcoming</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>3</td>
+                            <td>Oprec Pimpinan Genus 2022</td>
+                            <td>09-04-2022</td>
+                            <td>22-04-2022</td>
+                            <td>
+                            <button type="button" class="btn btn-primary btn-sm">Running</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>4</td>
+                            <td>Oprec Staff OMI 2020</td>
+                            <td>13-11-2020</td>
+                            <td>26-11-2020</td>
+                            <td>
+                            <button type="button" class="btn btn-danger btn-sm">Canceled</button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
 
