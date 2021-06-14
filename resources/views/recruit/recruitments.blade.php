8252fe6c111752e7767e34e6f5f894336ab51a06
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2021-06-14 15:09:18 +0700

    Tabel Recruitment

--- resources/views/recruit/recruitments.blade.php
+++ resources/views/recruit/recruitments.blade.php
@@ -8,8 +8,35 @@
         <div class="container">
             <h2 class="container">Recruitments</h2>
             <div class="container">Data pembukaan pendaftaran yang telah dibuat </div>
-                <a href="/" class="btn btn-primary">New Open Reacruitment</a>
-             </div>
+                <div style = "position:relative; left: 650px;">
+                    <a href="/" class="btn btn-primary">New Open Reacruitment</a>
+                </div>
+
+                <table class="table my-3">
+                    <thead class="table-info">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Acara</th>
+                        <th scope="col">Waktu Pembukaan</th>
+                        <th scope="col">Waktu Penutupan</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                    </thead>
+        
+                    <tbody>
+
+                    </tr>
+                    <td>1</td>
+                    <td>Panitia MPKMB 59</td>
+                    <td>16-02-2022</td>
+                    <td>16-03-2022</td>
+                    <td>
+                    <button type="button" class="btn btn-success btn-sm">Complete</button>
+                    </td>
+                    <tr>
+                    </tbody>
+                </table>
+
         <div class="b-example-divider"></div>
     </div>
 </div>
