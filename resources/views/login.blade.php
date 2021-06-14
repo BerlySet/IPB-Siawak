5f96c0965ad3a726835beb8e18b8e882ce0d1ff1
yube_11 <yudha_11@apps.ipb.ac.id>
2021-06-15 00:41:23 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,102 +21,107 @@
 
     <body class="bg-primary">
     <div class="bg">
-        <div class="kotaka"></div>
-        <div class="kotakb">
-            <div class="teksb">
-                <br>
-                <br>
-                    <center><h4 style="color:#FFFFFF">Sistem Informasi Anggota Ormawa dan Kepanitiaan IPB</h4></center>
-                </br>
-                </br>
-        </div>
-        </div>
-        <div class="kotak"></div>
-        <div class="logo"></div>
-            <div id="layoutAuthentication">
-                <div id="layoutAuthentication_content">
-                    <main>
-                        <div class="container align-middle">
-                            <div class="row justify-content-right">
-                                <div class="col-lg-5">
-                                    <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                        {{-- Error Alert --}}
-                                        @if(session('error'))
-                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <div class="kotaka">
+            <div class="logo"></div>
+            <div class="kotak">
+                <div class="foto"></div>
+                <div class="scale">
+                    <div id="layoutAuthentication">
+                        <div id="layoutAuthentication_content">
+                            <main>
+                                <div class="container">
+                                    <div class="row justify-content-right">
+                                        <div class="col-lg-5">
+                                            <div class="card shadow-lg border-0 rounded-lg mt-4">
+                                            {{-- Error Alert --}}
+                                            
+                                            @if(session('error'))
+                                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                             {{session('error')}}
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        @endif
-                                        <div class="card-header">
-                                            <h3 class="text-center font-weight-bolder my-4">LOGIN SIAWAK</h3>
-                                        </div>
-                                        <div class="card-body">
-                                            <form action="{{url('proses_login')}}" method="POST" id="logForm">
-                                                {{ csrf_field() }}
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                                
+                                            @endif
+                                            <div class="card-header">
+                                                <h2 class="text-center font-weight-bold my-4">LOGIN SIAWAK</h2>
+                                            </div>
+                                                
+                                            <div class="card-body">
+                                                <form action="{{url('proses_login')}}" method="POST" id="logForm">
+                                                {{ csrf_field() }}  
                                                 <div class="form-group">
                                                     @error('login_gagal')
-                                                        {{-- <span class="invalid-feedback" role="alert">
+                                                    {{--<span class="invalid-feedback" role="alert">
                                                             <strong>{{ $message }}</strong>
                                                         </span> --}}
+                                                        
                                                         <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                                            {{-- <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span> --}}
-                                                            <span class="alert-inner--text"><strong>Warning!</strong> {{ $message }}</span>
-                                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                                                <span aria-hidden="true">&times;</span>
-                                                            </button>
+                                                            {{--<span class="alert-inner--icon"><i class="ni ni-like-2"></i></span> --}}
+                                                                <span class="alert-inner--text"><strong>Warning!</strong> {{ $message }}</span>
+                                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
                                                         </div>
-                                                        @enderror
-                                                    <label class="small mb-1" for="inputEmailAddress">Email</label>
+                                                    @enderror
+                                                    <label class="medium mb-1" for="inputEmailAddress"><b>Email<b></label>
                                                     <input
                                                         class="form-control py-4"
                                                         id="inputEmailAddress"
                                                         name="email"
                                                         type="text"
                                                         placeholder="Masukkan Email"/>
-                                                    @if($errors->has('email'))
-                                                    <span style="color:red" class="error">{{ $errors->first('email') }}</span>
-                                                    @endif
+                                                        
+                                                        @if($errors->has('email'))
+                                                        <span style="color:red" class="error">{{ $errors->first('email') }}</span>
+                                                        @endif
                                                 </div>
+                                                
                                                 <div class="form-group">
-                                                    <label class="small mb-1" for="inputPassword">Password</label>
+                                                    <label class="medium mb-1" for="inputPassword"><b>Password</b></label>
                                                     <input
                                                         class="form-control py-4"
                                                         id="inputPassword"
                                                         type="password"
                                                         name="password"
                                                         placeholder="Masukkan Password"/>
-                                                    @if($errors->has('password'))
-                                                    <span style="color:red" class="error">{{ $errors->first('password') }}</span>
-                                                    @endif
+                                                        
+                                                        @if($errors->has('password'))
+                                                        <span style="color:red" class="error">{{ $errors->first('password') }}</span>
+                                                        @endif
                                                 </div>
+                                                
                                                 <div class="form-group">
                                                     <div class="custom-control custom-checkbox">
                                                         <input class="custom-control-input" id="rememberPasswordCheck" type="checkbox"/>
-                                                        <label class="custom-control-label" for="rememberPasswordCheck">Ingat Password</label>
+                                                        <label class="custom-control-label" for="rememberPasswordCheck"><b>Ingat Password?</b></label>
                                                     </div>
                                                 </div>
-                                                <div
-                                                    class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                
+                                                <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
                                                     {{-- <a class="small" href="#">Forgot Password?</a> --}}
-                                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                                    <button class="btn btn-primary btn-block" type="submit"><b>Login</b></button>
                                                 </div>
-                                            </form>
-                                        </div>
-                                        <div class="card-footer text-center">
-                                            <div class="small">
-                                                {{-- <a href="{{url('register')}}">Belum Punya Akun? Daftar!</a> --}}
+                                                
+                                                </form>
+                                            </div>
+                                            
+                                            <div class="card-footer text-center">
+                                                <div class="small">
+                                                    {{-- <a href="{{url('register')}}">Belum Punya Akun? Daftar!</a> --}}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </main>
                         </div>
-                    </main>
+                    </div>
                 </div>
             </div> 
         </div>
+    </div>
         <script
             src="https://code.jquery.com/jquery-3.4.1.min.js"
             crossorigin="anonymous"></script>
