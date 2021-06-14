4eaab0968c1afef6a7f745e1781d7af0dc1d500a
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2021-06-14 14:10:55 +0700

    Button NewRecruiment

--- resources/views/recruit/recruitments.blade.php
+++ resources/views/recruit/recruitments.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'Reacruitments')
 
 @section('container')
-<div>
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <body class="antialiased">
-       <h2>ini halaman recruitments.</h2>
-       <h3>Judul Oprec 1: {{$recruitment[0]->judul}} </h3>
-    </body>
-</html>
-
+<div class="overflow-auto w-100">
+    <div class=".d-flex mx-5 my-3">
+        <div class="container">
+            <h2 class="container">Recruitments</h2>
+            <div class="container">Data pembukaan pendaftaran yang telah dibuat </div>
+                <a href="/" class="btn btn-primary">New Open Reacruitment</a>
+             </div>
+        <div class="b-example-divider"></div>
+    </div>
 </div>
 @endsection
 
