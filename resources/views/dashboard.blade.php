59ae99cbb5fa9b8b097bdbfcc5c681f8a44e0756
anggario21 <anggario_21@apps.ipb.ac.id>
2021-06-15 22:50:09 +0700

    benerin dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,20 @@
         <h2 class="container">Dashboard</h2>
             <div class="container-fluid">
                 <div class="row">
-                    <h5>Selamat datang, <?php echo $user->nama; ?></h5>
-                    <p>Silahkan pilih menu.</p>
+                    <div class="col-md-12" style="padding:10px 15px 10px; border: 1px solid Lightgray; margin-bottom:10px;">
+                        <h5>Selamat datang, <?php echo $user->nama; ?></h5>
+                        <p>Silahkan pilih menu.</p>
+                    </div>
                     <div class="card">
                         <div class="card-body">
-                        <div class="row">
+                        <div class="row features-icons" style="padding:50px 0px 50px;">
                             <div class="col text-center">
                                     <div class="features-icons-icon text-center">
                                     <a href="/recruitments">
                                         <svg class="align-middle" width="90" height="90"><use xlink:href="#speedometer2"/></svg>
                                     </a>
                                     </div>
+                                    <br>
                                     <p>Lihat atau tambahkan pendaftaran kepengurusan atau acara.</p>
                             </div>    
                             <div class="col text-center">
@@ -28,6 +31,7 @@
                                         <svg class="align-middle" width="90" height="90"><use xlink:href="#table"/></svg>
                                     </a>
                                     </div>
+                                    <br>
                                     <p>Lihat atau edit posisi, status dan divisi pengurus.</p>
                             </div>
                             <div class="col text-center">
@@ -36,6 +40,7 @@
                                         <svg class="align-middle" width="90" height="90"><use xlink:href="#grid"/></svg>
                                     </a>
                                     </div>
+                                    <br>
                                     <p>Lihat atau isi SKPI dari kepengurusan atau acara.</p>
                             </div>
                         </div>
